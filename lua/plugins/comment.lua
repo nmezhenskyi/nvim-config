a257d6450e8393e73ecf1de858130f4c670808d0
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2024-04-10 14:44:17 -0400

    feat: add comment.nvim and update deps

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,10 @@
+return {
+   "numToStr/Comment.nvim",
+   opts = {
+      -- options go here:
+   },
+   config = function()
+      local comment = require("Comment").setup()
+   end,
+   lazy = false,
+}
